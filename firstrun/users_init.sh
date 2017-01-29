76241a78a341124c4e30e91637d5d33db9e2afba
Ronnie McGrog <kuzin.wlad@gmail.com>
2017-01-29 23:17:16 +0300

    Create users_init.sh
    пример приватных настроек

---
+++ firstrun/users_init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+## start private data ##
+dev1=dev1 # developer1
+pass1=pass1
+skey1=skey1
+
+dev2=dev2 # developer2
+pass2=pass2
+skey2=skey2
+
+robot=robot # script agent
+skey3=skey3
+pass3=pass3
+
+owner1=owner1 # product owner1
+skey4=skey4
+pass4=pass4
+
+owner2=owner2 # product owner2
+skey5=skey5
+pass5=pass5
+
+tuser=stub # юзер для быстрого и небезопасного подключения по команде su enot -
+pass6=pass6
+## end private data ##
